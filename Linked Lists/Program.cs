3e26f795c3eaa67c3c01283fcc6de0a465a16430
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2023-02-13 11:28:45 +0000

    Initial commit

---
+++ Linked Lists/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LinkedLists // Note: actual namespace depends on the project name.
+{
+    internal class LL<T>
+    {
+        private Node head = null;
+
+        private class Node
+        {
+            internal T data;
+            internal Node next;
+
+            internal Node(T value)
+            {
+                this.data = value;
+                this.next = null;
+            }
+        }
+
+        public LL()
+        {
+            this.head = null;
+        }
+
+        public void insertFront(T n)
+        {
+            Node newNode = new Node(n);
+            newNode.next = this.head;
+            this.head = newNode;
+            
+        }
+
+        public void Add(T n)
+        {
+            Node new_node = new Node(n);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
