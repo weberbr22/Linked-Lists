aaf7f9cbbe5f03df3229180e83a13d4a7f867425
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2023-02-20 21:17:00 +0000

    Update Program.cs

--- Linked Lists/Program.cs
+++ Linked Lists/Program.cs
@@ -1,46 +1,130 @@
 ﻿using System;
+using System.Text;
 
 namespace LinkedLists // Note: actual namespace depends on the project name.
 {
-    internal class LL<T>
+    public class IntegerLinkedList
     {
-        private Node head = null;
+        private Node head;
+        private int count;
+
+        public IntegerLinkedList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public void Append(int value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+            count++;
+        }
+
+        public bool Remove(int value)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+            if (head.Value == value)
+            {
+                head = head.Next;
+                count--;
+                return true;
+            }
+            Node current = head;
+            while (current.Next != null)
+            {
+                if (current.Next.Value == value)
+                {
+                    current.Next = current.Next.Next;
+                    count--;
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public int Sum()
+        {
+            int sum = 0;
+            Node current = head;
+            while (current != null)
+            {
+                sum += current.Value;
+                current = current.Next;
+            }
+            return sum;
+        }
+
+        public int Length()
+        {
+            return count;
+        }
+
+        public void Join(IntegerLinkedList otherList)
+        {
+            if (otherList == null || otherList.head == null)
+            {
+                return;
+            }
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = otherList.head;
+            count += otherList.count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            Node current = head;
+            while (current != null)
+            {
+                sb.Append(current.Value);
+                if (current.Next != null)
+                {
+                    sb.Append(", ");
+                }
+                current = current.Next;
+            }
+            sb.Append("]");
+            return sb.ToString();
+        }
 
         private class Node
         {
-            internal T data;
-            internal Node next;
+            public int Value;
+            public Node Next;
 
-            internal Node(T value)
+            public Node(int value)
             {
-                this.data = value;
-                this.next = null;
+                Value = value;
+                Next = null;
             }
-        }
-
-        public LL()
-        {
-            this.head = null;
-        }
-
-        public void insertFront(T n)
-        {
-            Node newNode = new Node(n);
-            newNode.next = this.head;
-            this.head = newNode;
-            
-        }
-
-        public void Add(T n)
-        {
-            Node new_node = new Node(n);
-
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+
         }
     }
 }
